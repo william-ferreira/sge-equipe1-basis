fbc16d9e39bb22f820b185c715b86c7411ac5b1e
William <williamferreiragt@gmail.com>
2021-01-12 15:19:55 -0300

    Primeiro commit. Setup do projeto.

---
+++ backend/sge/src/test/java/com/basis/sge/sge/SgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basis.sge.sge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
