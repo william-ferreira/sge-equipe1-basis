5bcd6105a23e457fb49fbf40f02c769f7607e9eb
Edson <edsonfilho734@gmail.com>
2021-01-14 13:53:02 -0300

    Criacao das entidades InscricaoResposta e Pergunta

---
+++ backend/sge/src/main/java/com/basis/sge/sge/dominio/InscricaoResposta.java
@@ -0,0 +1,2 @@
+package com.basis.sge.sge.dominio;public class InscricaoResposta {
+}
---
+++ backend/sge/src/main/java/com/basis/sge/sge/dominio/Pergunta.java
@@ -0,0 +1,11 @@
+package com.basis.sge.sge.dominio;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+//
+public class Pergunta {
+
+}
