b52515bc63faed0a83b492b4e7a44df56e681cd2
Edson <edsonfilho734@gmail.com>
2021-01-14 13:58:04 -0300

    Implementação de tabelas

--- backend/sge/src/main/java/com/basis/sge/sge/dominio/InscricaoResposta.java
+++ backend/sge/src/main/java/com/basis/sge/sge/dominio/InscricaoResposta.java
@@ -1,2 +1,30 @@
-package com.basis.sge.sge.dominio;public class InscricaoResposta {
+package com.basis.sge.sge.dominio;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Table(name = "inscricao_resposta")
+public class InscricaoResposta {
+
+    @Id
+    @Column(name = "id_inscricao")
+    private int idInscricao;
+
+    @Id
+    @Column(name = "id_evento")
+    private int idEvento;
+
+    @Id
+    @Column(name = "id_pergunta")
+    private int idPergunta;
+
+    @Column(name = "resposta")
+    private String resposta;
 }
--- backend/sge/src/main/java/com/basis/sge/sge/dominio/Pergunta.java
+++ backend/sge/src/main/java/com/basis/sge/sge/dominio/Pergunta.java
@@ -3,9 +3,32 @@ package com.basis.sge.sge.dominio;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.List;
+
+@Table(name = "pergunta")
 @Getter
 @Setter
-//
-public class Pergunta {
+public class Pergunta implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sq_pergunta")
+    @SequenceGenerator(name = "sq_pergunta", allocationSize = 1, sequenceName = "sq_pergunta")
+    private int id;
+
+    @Column(name = "titulo")
+    private String titulo;
+
+    @Column(name = "obrigatoriedade")
+    private boolean obrigatoriedade;
+
+    //@ManyToMany()
+    //private List<Evento> eventos;
 }
