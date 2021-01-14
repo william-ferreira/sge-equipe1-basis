73cfa4756304d561277ce6d9441d29eefefc7ac5
jaec35515@gmail.com <jaec35515@gmail.com>
2021-01-14 17:14:04 -0300

    modificação para receber um pull

---
+++ backend/sge/src/main/java/com/basis/sge/sge/servico/EventoServico.java
@@ -0,0 +1,12 @@
+package com.basis.sge.sge.servico;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class EventoServico {
+}
---
+++ backend/sge/src/main/java/com/basis/sge/sge/servico/mapper/EventoMapper.java
@@ -0,0 +1,7 @@
+package com.basis.sge.sge.servico.mapper;
+
+import com.basis.sge.sge.dominio.Evento;
+import com.basis.sge.sge.servico.dto.EventoDTO;
+
+public interface EventoMapper extends EntityMapper<EventoDTO, Evento>{
+}
