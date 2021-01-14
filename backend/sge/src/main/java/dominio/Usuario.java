529a7a268e159c727820cdb5bce88c4a5476d095
William <williamferreiragt@gmail.com>
2021-01-14 13:52:51 -0300

    Implementada a classe de usuario.

---
+++ backend/sge/src/main/java/dominio/Usuario.java
@@ -0,0 +1,41 @@
+package dominio;
+
+import liquibase.pro.packaged.id;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "usuario")
+@Getter
+@Setter
+
+public class Usuario implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sq_usuario")
+    @SequenceGenerator(name = "sq_usuario", allocationSize = 1, sequenceName = "sq_usuario")
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "cpf")
+    private String cpf;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "telefone")
+    private String telefone;
+
+    @Column(name = "data_nascimento")
+    private String dataNascimento;
+
+    @Column(name = "chave_usuario")
+    private String chaveUsuario;
+
+}
