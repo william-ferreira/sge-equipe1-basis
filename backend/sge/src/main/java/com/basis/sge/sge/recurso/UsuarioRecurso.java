83be04ddde9d45149d193e032a9f8f76e7222765
jaec35515@gmail.com <jaec35515@gmail.com>
2021-01-18 17:59:57 -0300

    completado o CRUD de usuario, falta testar

--- backend/sge/src/main/java/com/basis/sge/sge/recurso/UsuarioRecurso.java
+++ backend/sge/src/main/java/com/basis/sge/sge/recurso/UsuarioRecurso.java
@@ -1,47 +1,46 @@
-//package com.basis.sge.sge.recurso;
-//
-//import com.basis.sge.sge.servico.UsuarioServico;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//import com.basis.sge.sge.servico.dto.UsuarioDTO;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/api/usuario")
-//@RequiredArgsConstructor
-//public class UsuarioRecurso {
-//
-//    private final UsuarioServico usuarioServico;
-//
-//    @GetMapping
-//    public ResponseEntity<List<UsuarioDTO>> listar() {
-//        List list = usuarioServico.listar();
-//        return ResponseEntity.ok(list);
-//    }
-//
-//    @GetMapping("{/id}")
-//    public ResponseEntity<UsuarioDTO> obterPorID(@PathVariable Integer id) {
-//        UsuarioDTO usuarioDTO = usuarioServico.obterPorId(id);
-//        return ResponseEntity.ok(usuarioDTO);
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity<UsuarioDTO> salvar(@RequestBody UsuarioDTO usuarioDTO) {
-//        UsuarioDTO usuario = usuarioServico.salvar(usuarioDTO);
-//        return ResponseEntity.ok(usuario);
-//    }
-//
-//    @PutMapping
-//    public ResponseEntity<UsuarioDTO> editar(@RequestBody UsuarioDTO usuarioDTO) {
-//        return null;
-//    }
-//
-//    @DeleteMapping("{/id}")
-//    public void remover(@PathVariable Integer id) {
-//        //return ResponseEntity.ok().build();
-//    }
-//
-//}
+package com.basis.sge.sge.recurso;
+
+import com.basis.sge.sge.servico.UsuarioServico;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.basis.sge.sge.servico.dto.UsuarioDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/usuario")
+@RequiredArgsConstructor
+public class UsuarioRecurso {
+
+    private final UsuarioServico usuarioServico;
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioDTO>> listar() {
+        return ResponseEntity.ok(usuarioServico.listar());
+    }
+
+    @GetMapping("{/id}")
+    public ResponseEntity<UsuarioDTO> obterPorID(@PathVariable Integer id) {
+        UsuarioDTO usuarioDTO = usuarioServico.obterPorId(id);
+        return ResponseEntity.ok(usuarioDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<UsuarioDTO> salvar(@RequestBody UsuarioDTO usuarioDTO) {
+        UsuarioDTO usuario = usuarioServico.salvar(usuarioDTO);
+        return ResponseEntity.ok(usuario);
+    }
+
+    @PutMapping
+    public ResponseEntity<UsuarioDTO> atualizar(@RequestBody UsuarioDTO usuarioDTO) {
+        return ResponseEntity.ok(usuarioServico.atualizar(usuarioDTO));
+    }
+
+    @DeleteMapping("{/id}")
+    public void deletar(@PathVariable Integer id) {
+        usuarioServico.deletar(id);
+    }
+
+}
--- backend/sge/src/main/java/com/basis/sge/sge/servico/UsuarioServico.java
+++ backend/sge/src/main/java/com/basis/sge/sge/servico/UsuarioServico.java
@@ -1,29 +1,45 @@
-//package com.basis.sge.sge.servico;
-//
-//import com.sun.xml.bind.v2.TODO;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//import com.basis.sge.sge.servico.dto.UsuarioDTO;
-//
-//import javax.transaction.Transactional;
-//import java.util.List;
-//
-//@Service
-//@Transactional
-//@RequiredArgsConstructor
-//public class UsuarioServico {
-//
-//    public List<UsuarioDTO> listar() {
-//        return null;
-//    }
-//
-//    public UsuarioDTO obterPorId(Integer id) {
-//        return null;
-//    }
-//
-//    public UsuarioDTO salvar(UsuarioDTO usuarioDTO) {
-//        return null;
-//    }
-//
-//    // TODO: métodos para editar e remover
-//}
+package com.basis.sge.sge.servico;
+
+import com.basis.sge.sge.dominio.Usuario;
+import com.basis.sge.sge.repositorio.UsuarioRepositorio;
+import com.basis.sge.sge.servico.exception.RegraNegocioException;
+import com.basis.sge.sge.servico.mapper.UsuarioMapper;
+import com.sun.xml.bind.v2.TODO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import com.basis.sge.sge.servico.dto.UsuarioDTO;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UsuarioServico {
+    private final UsuarioRepositorio usuarioRepositorio;
+    private final UsuarioMapper usuarioMapper;
+
+    public List<UsuarioDTO> listar() {
+        return usuarioMapper.toDto(usuarioRepositorio.findAll());
+    }
+
+    public UsuarioDTO obterPorId(Integer id) {
+        Usuario user = usuarioRepositorio.findById(id)
+                .orElseThrow(()-> new RegraNegocioException("Id do usuário não encontrado"));
+        return null;
+    }
+
+    public UsuarioDTO salvar(UsuarioDTO usuarioDTO) {
+        usuarioRepositorio.save(usuarioMapper.toEntity(usuarioDTO));
+        return usuarioDTO;
+    }
+
+    public UsuarioDTO atualizar(UsuarioDTO usuarioDTO){
+        usuarioRepositorio.save(usuarioMapper.toEntity(usuarioDTO));
+        return usuarioDTO;
+    }
+
+    public void deletar(Integer id){
+        usuarioRepositorio.deleteById(id);
+    }
+}
