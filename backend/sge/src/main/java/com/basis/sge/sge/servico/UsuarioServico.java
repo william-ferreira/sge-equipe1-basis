ab6d5b731be9c3bce9b3cd9a0c7a3f5ce7743e2e
jaec35515@gmail.com <jaec35515@gmail.com>
2021-01-19 16:50:12 -0300

    realizado o envio de email quando for salvar um novo usuario

--- backend/sge/src/main/java/com/basis/sge/sge/servico/UsuarioServico.java
+++ backend/sge/src/main/java/com/basis/sge/sge/servico/UsuarioServico.java
@@ -2,9 +2,10 @@ package com.basis.sge.sge.servico;
 
 import com.basis.sge.sge.dominio.Usuario;
 import com.basis.sge.sge.repositorio.UsuarioRepositorio;
+import com.basis.sge.sge.servico.dto.EmailDTO;
 import com.basis.sge.sge.servico.exception.RegraNegocioException;
 import com.basis.sge.sge.servico.mapper.UsuarioMapper;
-import com.sun.xml.bind.v2.TODO;
+import com.basis.sge.sge.utils.EmailUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import com.basis.sge.sge.servico.dto.UsuarioDTO;
@@ -19,28 +20,39 @@ import java.util.UUID;
 public class UsuarioServico {
     private final UsuarioRepositorio usuarioRepositorio;
     private final UsuarioMapper usuarioMapper;
-    private String chave;
+    private final EmailUtils emailUtils;
+
 
     public List<UsuarioDTO> listar() {
         return usuarioMapper.toDto(usuarioRepositorio.findAll());
     }
 
     public UsuarioDTO obterPorId(Integer id) {
-        Usuario user = usuarioRepositorio.findById(id)
-                .orElseThrow(()-> new RegraNegocioException("Id do usuário não encontrado"));
+        Usuario user = buscar(id);
         return usuarioMapper.toDto(user);
     }
 
+    public Usuario buscar(Integer id){
+        return usuarioRepositorio.findById(id)
+                .orElseThrow(()-> new RegraNegocioException("Id do usuário não encontrado"));
+    }
+
     public UsuarioDTO salvar(UsuarioDTO usuarioDTO) {
-        Usuario user = usuarioRepositorio.save(usuarioMapper.toEntity(usuarioDTO));
+        Usuario user = usuarioMapper.toEntity(usuarioDTO);
         user.setChaveUsuario(UUID.randomUUID().toString());
-        chave = user.getChaveUsuario();
+        usuarioRepositorio.save(user);
+
+        String mensagem = "Email de testes";
+
+        emailUtils.enviarEmail(user.getEmail(), mensagem, "Cadastro do Usuário");
         return usuarioMapper.toDto(user);
     }
 
     public UsuarioDTO atualizar(UsuarioDTO usuarioDTO){
-        Usuario user = usuarioRepositorio.save(usuarioMapper.toEntity(usuarioDTO));
-        user.setChaveUsuario(chave);
+        Usuario userSalvo = buscar(usuarioDTO.getId());
+        Usuario user = usuarioMapper.toEntity(usuarioDTO);
+        user.setChaveUsuario(userSalvo.getChaveUsuario());
+        usuarioRepositorio.save(user);
         return usuarioMapper.toDto(user);
     }
 
