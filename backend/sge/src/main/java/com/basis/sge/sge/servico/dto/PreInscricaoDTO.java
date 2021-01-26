b8507b0f6350084eaec215b6d023d9a3c7235880
William <williamferreiragt@gmail.com>
2021-01-26 12:09:36 -0300

    Resolvendo conflito de merge com sprint-2.

--- backend/sge/src/main/java/com/basis/sge/sge/servico/dto/PreInscricaoDTO.java
+++ backend/sge/src/main/java/com/basis/sge/sge/servico/dto/PreInscricaoDTO.java
@@ -7,16 +7,21 @@ import com.basis.sge.sge.dominio.Usuario;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.List;
 
 @Getter
 @Setter
 public class PreInscricaoDTO implements Serializable {
-
+    @NotNull(message = "insira um id de Pré Inscrição válido")
     private Integer id;
+    @NotNull(message = "insira um id de Tipo de Situação válida")
     private Integer idTipoSituacao;
+    @NotNull(message = "insira um id de Tipo de Usuário válido")
     private Integer idUsuario;
+    @NotNull(message = "insira um id de Tipo de Evento válido")
     private Integer idEvento;
+    @NotNull(message = "A Lista não pode ser Nula")
     private List<InscricaoRespostaDTO> respostas;
 }
